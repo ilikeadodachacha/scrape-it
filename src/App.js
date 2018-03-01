ca774f6513072eaf65e02366f075ba50f6d97dd7
Landon Roberts <landonmarkroberts@gmail.com>
2018-03-01 17:55:09 -0600

    create input form

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+/* eslint-disable */
 class App extends React.Component {
   constructor(props) {
     super(props);
@@ -10,11 +10,17 @@ class App extends React.Component {
 
   render() {
     return (
-      <ul>
-        {this.state.quotes.map((quote, i) => {
-          return <li key={i}>{quote}</li>
-        })}
-      </ul>
+      <div>
+        <form className="Search-from">
+          Enter a search term: <input type="text" name="search-term"/><br/>
+          <input type="submit"/>
+        </form>
+        <ul>
+          {this.state.quotes.map((quote, i) => {
+            return <li key={i}>{quote}</li>
+          })}
+        </ul>
+      </div>
     );
   }
 }
