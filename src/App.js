46a63672f707a9200345dcb793a749d779681cfa
Landon Roberts <landonmarkroberts@gmail.com>
2018-03-02 12:01:24 -0600

    Accept search input

--- src/App.js
+++ src/App.js
@@ -5,15 +5,31 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      searchTerm: '',
       quotes: this.props.quotes.messages.non_personalized,
     }
+    bindAll(this, 'handleChange', 'goScraping');
+  }
+
+  handleChange(e) {
+    e.preventDefault();
+    const thisState = this.state;
+    thisState.searchTerm = e.target.value;
+    this.setState({
+      searchTerm: thisState.searchTerm
+    })
+  }
+
+  goScraping(e) {
+    e.preventDefault();
+    console.log(this.state.searchTerm)
   }
 
   render() {
     return (
       <div>
-        <form className="Search-form">
-          Enter a search term: <input type="text" name="search-term"/><br/>
+        <form className="Search-form" onSubmit={this.goScraping}>
+          Enter a search term: <input type="text" name="search-term" value={this.state.searchTerm} onChange={this.handleChange}/><br/>
           <input type="submit" value="Scrape-it"/>
         </form>
         <ul>
